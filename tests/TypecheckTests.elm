3039125e7a12b8eeabb253fe9095139f72aabf86
Alex Shadley <shadleyalex@gmail.com>
2019-04-03 12:45:17 -0500

    fixes typecheck tests

--- tests/TypecheckTests.elm
+++ tests/TypecheckTests.elm
@@ -4,7 +4,7 @@ import Expect exposing (Expectation)
 import Fuzz exposing (Fuzzer, int, list, string)
 import Test exposing (..)
 
-import Typecheck exposing (CheckResult(..), typecheck)
+import Typecheck exposing (CheckResult(..), VType(..), typecheck)
 import Types exposing (..)
 
 -- code repition; this should be refactored out in later commits
