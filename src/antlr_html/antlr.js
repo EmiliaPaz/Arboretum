fe67685c1249f0042d8963b7d6c2b7ed60b7247c
EmiliaPaz <emilia.paz@ku.edu>
2019-04-10 09:10:02 -0500

    elm javascript port working. Run elm make src/Main.elm --output=main.js and open index.html that includes main.js and ports.js

--- src/antlr_html/antlr.js
+++ src/antlr_html/antlr.js
@@ -1,31 +1,72 @@
-const http = require('http');
 const antlr4 = require('antlr4/index');
 const ArboretumLexer = require('./ArboretumLexer');
 const ArboretumParser = require('./ArboretumParser');
 const HtmlArboretumListener = require('./HtmlArboretumListener').HtmlArboretumListener;
 
-
-
-http.createServer((req, res) => {
+// http.createServer((req, res) => {
    
-   res.writeHead(200, {
-       'Content-Type': 'text/html',        
-   });
+//    res.writeHead(200, {
+//        'Content-Type': 'text/html',        
+//    });
 
-   res.write('<html><head><meta charset="UTF-8"/> </head><body>');
+//    res.write('<html><head><meta charset="UTF-8"/> </head><body>');
    
-   var input = "(3+5)-7";
-   var chars = new antlr4.InputStream(input);
-   var lexer = new ArboretumLexer.ArboretumLexer(chars);
-   var tokens  = new antlr4.CommonTokenStream(lexer);
-   var parser = new ArboretumParser.ArboretumParser(tokens);
+//    var input = "(3+5)-7";
+//    var chars = new antlr4.InputStream(input);
+//    var lexer = new ArboretumLexer.ArboretumLexer(chars);
+//    var tokens  = new antlr4.CommonTokenStream(lexer);
+//    var parser = new ArboretumParser.ArboretumParser(tokens);
+//    parser.buildParseTrees = true;   
+//    var tree = parser.finalExpression();   
+//    var htmlArboretum = new HtmlArboretumListener(res);
+
+//    antlr4.tree.ParseTreeWalker.DEFAULT.walk(htmlArboretum, tree);
+   
+//    res.write('</body></html>');
+//    res.end();
+
+// }).listen(1337);
+
+
+var app = Elm.Main.init({
+    node: document.getElementById('elm')
+});  // nah
+
+
+/*
+Methods we declared as 'port' in Elm will be available on app.ports that
+will get invoked/called when we send a cmd message out via Elm.
+*/
+app.ports.toJS.subscribe(function(msg) {
+    var input = "(3+5)-7";
+   var chars = new InputStream(input);
+   var lexer = new _ArboretumLexer(chars);
+   var tokens  = new CommonTokenStream(lexer);
+   var parser = new _ArboretumParser(tokens);
    parser.buildParseTrees = true;   
    var tree = parser.finalExpression();   
-   var htmlArboretum = new HtmlArboretumListener(res);
-
-   antlr4.tree.ParseTreeWalker.DEFAULT.walk(htmlArboretum, tree);
    
-   res.write('</body></html>');
-   res.end();
+   class Visitor {
+    visitChildren(ctx) {
+      if (!ctx) {
+        return;
+      }
+  
+      if (ctx.children) {
+        return ctx.children.map(child => {
+          if (child.children && child.children.length != 0) {
+            return child.accept(this);
+          } else {
+            return child.getText();
+          }
+        });
+      }
+    }
+  }
+  
+   tree.accept(new Visitor());
 
-}).listen(1337);
+
+   app.ports.toElm.send("message from js");        // Sending to the Elm sub
+
+});
