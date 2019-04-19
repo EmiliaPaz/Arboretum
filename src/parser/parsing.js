2640ffc304668b218caa3ab79be22dc44ae8e03f
Alex Shadley <shadleyalex@gmail.com>
2019-04-19 13:13:03 -0500

    adds more infrastructure, parser code

---
+++ src/parser/parsing.js
@@ -0,0 +1,136 @@
+"use strict"
+
+// Adding a Parser (grammar only, only reads the input without any actions).
+// Using the Token Vocabulary defined in the previous step.
+
+const scriptLexer = require("./lexing")
+const Parser = require("chevrotain").Parser
+const tokenVocabulary = scriptLexer.tokenVocabulary
+
+// individual imports, prefer ES6 imports if supported in your runtime/transpiler...
+const Identifier = tokenVocabulary.Identifier
+const Integer = tokenVocabulary.Integer
+const Boolean = tokenVocabulary.Boolean
+const Equivalence = tokenVocabulary.Equivalence
+const Assignment = tokenVocabulary.Assignment
+const Addition = tokenVocabulary.Addition
+const Subtraction = tokenVocabulary.Subtraction
+const Multiplication = tokenVocabulary.Multiplication
+const LogicalOR = tokenVocabulary.LogicalOR
+const LogicalAND = tokenVocabulary.LogicalAND
+const LParen = tokenVocabulary.LParen
+const RParen = tokenVocabulary.RParen
+
+// ----------------- parser -----------------
+class ScriptParser extends Parser {
+    // A config object as a constructor argument is normally not needed.
+    // Our tutorial scenario requires a dynamic configuration to support step3 without duplicating code.
+    constructor(config) {
+        super(tokenVocabulary, config)
+
+        // for conciseness
+        const $ = this
+
+        $.RULE("assignmentStatement", () => {
+            $.CONSUME(Identifier)
+            $.CONSUME(Assignment)
+            $.SUBRULE($.expression)
+        })
+
+        $.RULE("expression", () => {
+            $.SUBRULE($.eqExpression)
+        })
+
+        $.RULE("eqExpression", () => {
+            $.SUBRULE($.orExpression)
+            $.OPTION( () => {
+                $.CONSUME(Equivalence)
+                $.SUBRULE2($.orExpression)
+            })
+        })
+
+        $.RULE("orExpression", () => {
+            $.SUBRULE($.andExpression)
+            $.MANY( () => {
+                $.CONSUME(LogicalOR)
+                $.SUBRULE2($.andExpression)
+            })
+        })
+        $.RULE("andExpression", () => {
+            $.SUBRULE($.subtExpression)
+            $.MANY( () => {
+                $.CONSUME(LogicalAND)
+                $.SUBRULE2($.subtExpression)
+            })
+        })
+
+        $.RULE("subtExpression", () => {
+            $.SUBRULE($.addExpression)
+            $.MANY( () => {
+                $.CONSUME(Subtraction)
+                $.SUBRULE2($.addExpression)
+            })
+        })
+
+        $.RULE("addExpression", () => {
+            $.SUBRULE($.multExpression)
+            $.MANY( () => {
+                $.CONSUME(Addition)
+                $.SUBRULE2($.multExpression)
+            })
+        })
+
+        $.RULE("multExpression", () => {
+            $.SUBRULE($.atomicExpression)
+            $.MANY( () => {
+                $.CONSUME(Multiplication)
+                $.SUBRULE2($.atomicExpression)
+            })
+        })
+
+        $.RULE("atomicExpression", () => {
+            $.OR([
+                { ALT: () => {
+                    $.CONSUME(LParen)
+                    $.SUBRULE($.expression)
+                    $.CONSUME(RParen)
+                }},
+                { ALT: () => $.CONSUME(Integer) },
+                { ALT: () => $.CONSUME(Boolean) },
+                { ALT: () => $.CONSUME(Identifier) }
+            ])
+        })
+
+
+        // very important to call this after all the rules have been defined.
+        // otherwise the parser may not work correctly as it will lack information
+        // derived during the self analysis phase.
+        this.performSelfAnalysis()
+    }
+}
+
+// We only ever need one as the parser internal state is reset for each new input.
+const parserInstance = new ScriptParser()
+
+module.exports = {
+    parserInstance: parserInstance,
+
+    ScriptParser: ScriptParser,
+
+    parse: function(inputText) {
+        const lexResult = scriptLexer.lex(inputText)
+
+        // ".input" is a setter which will reset the parser's internal's state.
+        parserInstance.input = lexResult.tokens
+
+        // No semantic actions so this won't return anything yet.
+        parserInstance.assignmentStatement()
+
+        if (parserInstance.errors.length > 0) {
+            throw Error(
+                "Sad sad panda, parsing errors detected!\n" +
+                    parserInstance.errors[0].message
+            )
+        }
+    }
+}
---
+++ src/parser/visitor.js
@@ -0,0 +1,177 @@
+"use strict"
+
+
+const scriptLexer = require("./lexing")
+// re-using the parser implemented in step two.
+const parser = require("./parsing")
+const SelectParser = parser.ScriptParser
+
+// A new parser instance with CST output (enabled by default).
+const parserInstance = new SelectParser([])
+// The base visitor class can be accessed via the a parser instance.
+const BaseScriptVisitor = parserInstance.getBaseCstVisitorConstructor()
+
+
+class ToAstVisitor extends BaseScriptVisitor {
+    constructor() {
+        super()
+        this.validateVisitor()
+    }
+
+    assignmentStatement(ctx) {
+        const id = ctx.Identifier[0].image
+
+        const exp = this.visit(ctx.expression)
+
+        return {
+            type: "ASSIGN_STMT",
+            identifier: id,
+            expression: exp
+        }
+    }
+
+    // expression rule is some sort of wrapper right now
+    expression(ctx) {
+        const expr = this.visit(ctx.eqExpression)
+
+        return expr
+    }
+
+    /*
+    This and many of the following visitor rules check to see if more than one
+    subexpression exists to see if the rule was actually used.  If it was, a
+    json node is returned.  Otherwise, the results of visiting further down
+    the tree are returned.
+    */
+    eqExpression(ctx) {
+        if(ctx.orExpression.length > 1) {
+            const lhs = this.visit(ctx.orExpression[0])
+            const rhs = this.visit(ctx.orExpression[1])
+            return {
+                type: "EQ_EXPR",
+                lhs: lhs,
+                rhs: rhs,
+            }
+        }
+        else {
+            return this.visit(ctx.orExpression)
+        }
+    }
+
+    orExpression(ctx) {
+        if(ctx.andExpression.length > 1) {
+            const children = ctx.andExpression.map(node => this.visit(node))
+            return {
+                type: "OR_EXPR",
+                children: children,
+            }
+        }
+        else {
+            return this.visit(ctx.andExpression)
+        }
+    }
+
+    andExpression(ctx) {
+        if(ctx.subtExpression.length > 1) {
+            const children = ctx.subtExpression.map(node => this.visit(node))
+            return {
+                type: "AND_EXPR",
+                children: children,
+            }
+        }
+        else {
+            return this.visit(ctx.subtExpression)
+        }
+    }
+
+    subtExpression(ctx) {
+        if(ctx.addExpression.length > 1) {
+            const children = ctx.addExpression.map(node => this.visit(node))
+            return {
+                type: "SUBT_EXPR",
+                children: children,
+            }
+        }
+        else {
+            return this.visit(ctx.addExpression)
+        }
+    }
+
+    addExpression(ctx) {
+        if(ctx.multExpression.length > 1) {
+            const children = ctx.multExpression.map(node => this.visit(node))
+            return {
+                type: "ADD_EXPR",
+                children: children,
+            }
+        }
+        else {
+            return this.visit(ctx.multExpression)
+        }
+    }
+    
+    multExpression(ctx) {
+        if(ctx.atomicExpression.length > 1) {
+            const children = ctx.atomicExpression.map(node => this.visit(node))
+            return {
+                type: "MULT_EXPR",
+                children: children,
+            }
+        }
+        else {
+            return this.visit(ctx.atomicExpression)
+        }
+    }
+
+    atomicExpression(ctx) {
+        if(ctx.expression) {
+            return this.visit(ctx.expression)
+        }
+        else if(ctx.Integer) {
+            return {
+                type: "INT",
+                value: ctx.Integer[0].image,
+            }
+        }
+        else if(ctx.Boolean) {
+            return {
+                type: "BOOL",
+                value: ctx.Boolean[0].image,
+            }
+        }
+        else if(ctx.Identifier) {
+            return {
+                type: "ID",
+                value: ctx.Identifier[0].image,
+            }
+        }
+    }
+
+}
+
+
+// Our visitor has no state, so a single instance is sufficient.
+const toAstVisitorInstance = new ToAstVisitor()
+
+module.exports = {
+    toAst: function(inputText) {
+        const lexResult = scriptLexer.lex(inputText)
+
+        // ".input" is a setter which will reset the parser's internal's state.
+        parserInstance.input = lexResult.tokens
+
+        // Automatic CST created when parsing
+        const cst = parserInstance.assignmentStatement()
+
+        if (parserInstance.errors.length > 0) {
+            throw Error(
+                "Sad sad panda, parsing errors detected!\n" +
+                    parserInstance.errors[0].message
+            )
+        }
+
+        const ast = toAstVisitorInstance.visit(cst)
+
+        return ast
+    }
+}
